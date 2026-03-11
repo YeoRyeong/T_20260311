65142f4275d8d28b754d94d8f7db9db78919bfe5
Jun <tjrwns0012@naver.com>
2026-03-11 16:45:24 +0900

    동적 메모리 설명

--- main.cpp
+++ main.cpp
@@ -4,7 +4,19 @@
 using namespace std;
 
 int main()
-{	
+{
+	// computer 
+	// heap 동적으로 int 사이즈 만들고 주소 반환
+	int* Players = new int;
+
+	// 포인터 변수의 주소의 heap의 영역 반환
+	delete Players;
+	Players = nullptr;
+
+	if (Players) // 참인 경우 사용하고 아닌 경우 사용하지 말라는 형식으로
+	{
+
+	}
 
 	return 0;
 }
