f9da927909613a989da58ee05f0b712c9c4cbde9
Jun <tjrwns0012@naver.com>
2026-03-11 17:24:21 +0900

    힙 과제?

--- main.cpp
+++ main.cpp
@@ -5,18 +5,34 @@ using namespace std;
 
 int main()
 {
-	// computer memory unmanaged language / human managed language
-	// heap 동적으로 int 사이즈 만들고 주소 반환
-	int* Players = new int;
+	// 숫자 입력 받아서 그걸 크기로 숫자 배열을 만들어서
+	// 1부터 크기까지 초기화하고 출력하는 프로그램 만들어 보세요.
+	// heap 배열을 size 크기만큼 int, 시작번지(int*)
+
+	int Input = 0;
+
+	cin >> Input;
+
+	int* P = new int[Input];
+
+	if (P == nullptr) // if(P == nullptr), if(P == 0)
+	{
+		return -1;
+	}
+
+	for (int i = 0; i < Input; i++)
+	{
+		P[i] = i + 1; // -> 	P[i] = i + 1;
+	}
+
+	for (int i = 0; i < Input; i++)
+	{
+		cout << *(P + 1) << endl; // -> cout << *(P + 1) << endl
+	}
 
 	// 포인터 변수의 주소의 heap의 영역 반환
-	delete Players;
-	Players = nullptr;
-
-	if (Players) // 참인 경우 사용하고 아닌 경우 사용하지 말라는 형식으로
-	{
-
-	}
+	delete[] P;
+	P = nullptr;
 
 	return 0;
 }
