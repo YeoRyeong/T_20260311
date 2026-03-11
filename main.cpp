d3dac4872554c81db3835dd098e2bfcf50748cd0
Jun <tjrwns0012@naver.com>
2026-03-11 14:07:07 +0900

    포인터 수정

--- main.cpp
+++ main.cpp
@@ -12,23 +12,21 @@ void Length(char* Leng)
 		{
 			break;
 		}
-
-		Count++;
 	}
 
 	cout << "문자열의 갯수 : " << Count << endl;
 
-
 }
 
-void Change(char* Chan)
+void Change(char* Chan, char* Ch1, char* Ch2)
 {
 	cout << "특정문 바꾸기(l -> W) : ";
+
 	for (int j = 0; j < 6; j++)
 	{
-		if (Chan[j] == 'l')
+		if (Chan[j] == *Ch1)
 		{
-			Chan[j] = 'W';
+			Chan[j] = *Ch2;
 		}
 
 		cout << Chan[j];
@@ -36,11 +34,11 @@ void Change(char* Chan)
 
 }
 
-void Search(char* Sear)
+void Search(char* Sear, char* Se)
 {
 	for (int k = 0; k < 6; k++)
 	{
-		if (Sear[k] == 'o')
+		if (Sear[k] == *Se)
 		{
 			cout << "o의 위치 : " << k;
 		}
@@ -54,16 +52,22 @@ int main()
 	// 문자열 중에서 특정 문자를 특정문 바꾸기
 	// 문자열 중에서 특정 문자 위치 찾기
 
+	//	특정 값을 함수 인자로 작업
+
+
 	char Array[6] = {'H', 'e', 'l', 'l', 'o', '\0'};
+	char Ar1 = 'l';
+	char Ar2 = 'W';
+	char Se = 'o';
 
 	cout << "시작 : " << Array << endl;
 	
 	Length(Array);
-	Change(Array);
+	Change(Array, &Ar1, &Ar2);
 
 	cout << "\n";
 	
-	Search(Array);
+	Search(Array, &Se);
 
 	return 0;
 }
